94a38cfa278660bc37024300f1e41d74d67db1ed
Deepak Kumar <61252338+dkkumar77@users.noreply.github.com>
2021-05-10 22:48:18 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #define MAX_ARGUMENT 5
 #define MAX_BUFFER 200
-
 /**
  * Basic shell implementation by Deepak Kumar & Mani Shah
  * May 9th 2021
@@ -18,10 +17,18 @@ void append_errors(char * sequence [], int code, FILE * file);
 
 int main(int argc, char * argv[]) {
 
+    /* _________________________________________________________________________________ */
+    const char HISTORY_PATH = "/shellhistory";
     int status_code;
-    char buffer [256];
     FILE *hist_file;
-    hist_file = fopen("shellhistory.txt","a");
+    char directory_buffer[100];
+    char * final = getcwd(directory_buffer, sizeof(directory_buffer));
+    char * buff = malloc(sizeof(final)+sizeof(HISTORY_PATH));
+    strcpy(buff,final);
+    strcat(buff, HISTORY_PATH);
+    /* _________________________________________________________________________________ */
+
+    hist_file = fopen(buff,"a");
     if(hist_file == NULL){
         perror("input stream error");
     }
@@ -31,12 +38,13 @@ int main(int argc, char * argv[]) {
          */
         if(argc != 0){
         }
-        
+
         fclose(hist_file);
-        
+
+        free(buff);
+
     exit(0);
 }
-
 
 void append_history(char * sequence [], int arg_count, FILE * fpointer){
     for(int i = 0 ; i < arg_count; i++) {
@@ -46,5 +54,6 @@ void append_history(char * sequence [], int arg_count, FILE * fpointer){
 }
 void append_errors(char * sequence [], int code, FILE * fpointer){
     fprintf(fpointer, "ERROR:\n\n '%s' exited with code %d", sequence, code);
-    
+
+
 }
